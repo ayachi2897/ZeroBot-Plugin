daf6011266520d2872cbfa073858f8e1b7cd3f06
ayachi2897 <46046810+ayachi2897@users.noreply.github.com>
2023-08-18 01:11:45 +0800

    Update main.go

--- main.go
+++ main.go
@@ -256,8 +256,8 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	// sus = append(sus, 12345678)
-	// sus = append(sus, 87654321)
+	sus = append(sus, 1830206734)
+	sus = append(sus, 2865327589)
 
 	// 启用 webui
 	// go webctrl.RunGui(*g)
